823dec3a93fc993df9f2dacde23ae3f3f4bed023
Brendyn Dabrowski <bdabrowski@live.com>
2025-01-18 11:52:07 -0800

    initial commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "fuoco",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Networking",
+            targets: ["Networking"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk.git",
+            from: "11.6.0"
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Networking",
+            dependencies: [
+              .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
+              .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+            ]
+        ),
+    ]
+)
